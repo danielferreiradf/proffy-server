ba7d9e06fd40e9537253b390188b2a6aa8ad667a
danielferreiradf <danielferreiradk@gmail.com>
2020-08-06 10:05:04 +0100

    Added methods to list and create connections

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import ClassesController from "./controllers/ClassesController";
+import ConnectionsController from "./controllers/ConnectionsController";
 
 const routes = Router();
 const classesControllers = new ClassesController();
+const connectionsControllers = new ConnectionsController();
 
 routes.post("/classes", classesControllers.create);
 routes.get("/classes", classesControllers.index);
+
+routes.post("/connections", connectionsControllers.create);
+routes.get("/connections", connectionsControllers.index);
 
 export default routes;
